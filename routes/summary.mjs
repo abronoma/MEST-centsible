dfe795213bfd54342f67234f509f46a88a767323
abronoma <trudykingsberry@gmail.com>
2025-04-24 07:56:36 +0000

    added all changes

--- routes/summary.mjs
+++ routes/summary.mjs
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { isAuthenticated } from "../middlewares/auth.mjs";
+import { isAuthenticated, isAuthorized } from "../middlewares/auth.mjs";
 import { getSummary } from "../controllers/summary.mjs";
 import { getCategoryChartData, getMonthlyChartData } from "../utilities/summary.mjs";
+import { getDashboardData } from "../controllers/dashboard.mjs";
 
 
 const summaryRouter = Router();
 
 
-
+summaryRouter.get('/data', isAuthenticated, isAuthorized, getDashboardData);
 summaryRouter.get("/summary/", isAuthenticated, getSummary);
 summaryRouter.get('/charts/category', isAuthenticated, getCategoryChartData);
 summaryRouter.get('/charts/monthly', isAuthenticated, getMonthlyChartData);
